542b6278ea67b6e1bbd34024c942e06b80242b2b
dg1223 <alavi1223@hotmail.com>
2023-04-30 15:20:22 -0400

    created client side to receive phones data from server

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Main from "./components/Main.jsx";
+import Phones from "./components/Phones.jsx";
+import Phone from "./components/Phone";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+        loader: () => fetch("http://localhost:5000/phones"),
+      },
+      {
+        path: "/phone/:id",
+        element: <Phone></Phone>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/phones/${params.id}`),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
